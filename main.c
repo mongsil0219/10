ebc6322c7b97a734e677d0ffe8b98787855506aa
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-11-08 17:04:57 +0900

    05

--- main.c
+++ main.c
@@ -4,8 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void) {
-	char str[30] = "happy C programming";
+	FILE*fp;
+	char str[30];
+	int i;
+	fp = fopen("sample.txt", "w");
 	
-	printf("문자열 \"%s\"의 길이: %i",str,strlen(str));
+	printf("input a word:");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+
+	printf("input a word:");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+	
+	printf("input a word:");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+	
+	
+	fclose(fp);
+	
 	return 0;
 	}
